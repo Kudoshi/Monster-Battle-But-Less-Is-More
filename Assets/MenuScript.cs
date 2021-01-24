f8e93ffd777739604e9a4bbc862cdc4de551d126
Kudoshi <imbakudo@gmail.com>
2021-01-24 19:48:12 +0800

    First push

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject LevelsMenu;
+
+    public void play()
+    {
+        MainMenu.SetActive(false);
+        LevelsMenu.SetActive(true);
+    }
+
+    public void levelsback()
+    {
+        MainMenu.SetActive(true);
+        LevelsMenu.SetActive(false);
+    }
+}
